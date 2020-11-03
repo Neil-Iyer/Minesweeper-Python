79fcaa965ccfe3d4752b1fc5c17865c40da25ad3
Gavin McCabe <gavin@caltech.edu>
2020-11-02 19:21:49 -0500

    Update game with broken auto-expose, fix colors, and add the tile's number after left-click.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from tkinter import *
+import tkinter.messagebox
 import random
+
 
 
 tiles = [] # List of all tiles in the gam
 root = Tk() # Root window
 c = Canvas(root, width=0, height=0) # Canvas to hold the tiles
 size = (0, 0) # Size of the board (in tiles)
+num_bombs = 0 # Number of bombs on the board
+flagged_tiles = [] # List of tiles that are currently flagged
 
 
 class Tile:
@@ -14,6 +18,7 @@ class Tile:
 	tk_id = None # The tkinter id
 	is_bomb = False # The status of the tile as a bomb
 	is_flagged = False # The status of the tile as flagged
+	is_shown = False
 	num = 0 # Number of the tile (i.e. how many many neighbors are bombs)
 
 	def __init__(self, loc, tk_id):
@@ -21,8 +26,23 @@ class Tile:
 		self.loc = loc
 
 	def left_click(self):
-		print(self.num)
-		# TODO
+		"""
+		Handle the left-click event
+		:return: None
+		"""
+
+		if self.is_shown or self.is_flagged:
+			return
+
+		loc_x, loc_y = self.loc
+
+		c.create_text(loc_x * 50 + 25, loc_y * 50 + 25, text=self.num,
+					  font="Helvetica -25")
+		self.is_shown = True
+		c.itemconfig(self.tk_id, fill="gray")
+		if self.num == 0:
+			auto_expose(self)
+		check_loss(self)
 
 	def right_click(self):
 		"""
@@ -31,16 +51,21 @@ class Tile:
 		:return: None
 		"""
 
-		current_color = c.itemcget(self.tk_id, "fill")
-		if current_color != "orange":
+		global flagged_tiles
+
+		if not self.is_flagged:
 			self.is_flagged = True
 			c.itemconfig(self.tk_id, fill="orange")
+			root.update()
+			flagged_tiles.append(self)
 		elif self.is_bomb:
 			self.is_flagged = False
-			c.itemconfig(self.tk_id, fill="blue")
+			flagged_tiles.remove(self)
 		else:
 			self.is_flagged = False
-			c.itemconfig(self.tk_id, fill="red")
+			flagged_tiles.remove(self)
+
+		check_win()
 
 	def calculate_num(self):
 		"""
@@ -107,8 +132,89 @@ def get_tile_from_loc(loc):
 
 	for tile in tiles:
 		if tile.loc == loc:
-			print(f"{loc} - {tile.loc}")
 			return tile
+
+
+def check_loss(tile):
+	"""
+	Checks if the user has lost the game.  If the user has lost, display the
+	loss message
+	:param tile: The Tile object that has been pressed
+	:return: None
+	"""
+
+	if tile.is_bomb:
+		# Display loss message
+		tkinter.messagebox.showinfo(title="Minesweeper",
+									message="That was a bomb, you lose!")
+
+
+def check_win():
+	"""
+	Checks if the user has won the game.  If the user has won, display the
+	win message.
+	:return: None
+	"""
+
+	if num_bombs == len(flagged_tiles):
+		for tile in flagged_tiles:
+			if not tile.is_bomb:
+				return
+
+		# Display the win message
+		tkinter.messagebox.showinfo(title="Minesweeper",
+									message="Congratulations, you won!")
+
+
+def auto_expose(tile):
+	# TODO
+
+	"""
+	Automatically expose all neighboring zero tiles.
+	:param tile: The tile to run auto-exposure on
+	:return:
+	"""
+
+	# All possible transformations to go to each of the neighboring tiles
+	neighbor_moves = [
+		[-1, -1],
+		[0, -1],
+		[1, -1],
+		[-1, 0],
+		[1, 0],
+		[-1, 1],
+		[0, 1],
+		[1, 1]]
+
+	loc_x, loc_y = tile.loc
+
+	for move in neighbor_moves:
+
+		# Neighboring tile's position
+		neighbor_x = loc_x + move[0]
+		neighbor_y = loc_y + move[1]
+
+		total_width, total_height = size
+
+		# Make sure the tile exists in the board
+		if neighbor_x < 0 or neighbor_y < 0 or neighbor_x > total_width - \
+				1 or neighbor_y > total_height - 1:
+			continue
+
+		# Get the Tile object from the tile's location and update the sum
+		# as required.
+		neighbor_tile = get_tile_from_loc((neighbor_x, neighbor_y))
+		if neighbor_tile.num == 0:
+			neighbor_tile.left_click()
+		elif not neighbor_tile.is_bomb:
+			# TODO
+			loc_x, loc_y = neighbor_tile.loc
+			print(f"({loc_x}, {loc_y})")
+			c.create_text(loc_x * 50 + 25, loc_y * 50 + 25,
+						  text=neighbor_tile.num,
+						  font="Helvetica -25")
+			neighbor_tile.is_shown = True
+			c.itemconfig(neighbor_tile.tk_id, fill="gray")
 
 
 def click_event(event):
@@ -120,6 +226,8 @@ def click_event(event):
 
 	clicked_tile_id = c.find_closest(event.x, event.y)[0]
 	clicked_tile = get_tile_from_id(clicked_tile_id)
+	if clicked_tile is None:
+		return
 
 	if event.num == 1:
 		clicked_tile.left_click()
@@ -127,10 +235,9 @@ def click_event(event):
 		clicked_tile.right_click()
 
 
-def set_bombs(num_bombs):
+def set_bombs():
 	"""
 	Changes `num_bombs` tiles to be bombs
-	:param num_bombs: Number of tiles that should be set a bombs
 	:return: None
 	"""
 
@@ -142,7 +249,6 @@ def set_bombs(num_bombs):
 			bomb_tiles.append(rand_tile)
 
 	for tile in bomb_tiles:
-		c.itemconfig(tile.tk_id, fill="blue")
 		tile.is_bomb = True
 
 
@@ -168,7 +274,7 @@ def fill(width, height):
 	for i in range(height):
 		for j in range(width):
 			r = c.create_rectangle(j*50, i*50, j*50+50, i*50+50,
-										fill="red", outline="blue")
+										fill="white", outline="black")
 			tiles.append(Tile((j, i), r))
 
 
@@ -203,7 +309,7 @@ def main():
 	"""
 
 
-	global root, c, size
+	global root, c, size, num_bombs
 
 	# Get user input to initialize the game
 	width_board, height_board, num_bombs = get_initial_user_input()
@@ -215,7 +321,7 @@ def main():
 
 	# Get the GUI ready
 	fill(width_board, height_board)
-	set_bombs(num_bombs)
+	set_bombs()
 	calculate_nums()
 
 	# Handle button clicks
